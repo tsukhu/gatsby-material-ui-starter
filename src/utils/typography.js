89436712ff4d0c5842a709fa271159a6a5cb929a
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-22 09:48:52 +0530

    Github Theme added

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,9 @@
-import Typography from "typography";
-import kirkhamTheme from "typography-theme-kirkham";
+import Typography from 'typography';
+//import kirkhamTheme from 'typography-theme-kirkham';
+import githubTheme from 'typography-theme-github';
 
-const typography = new Typography(kirkhamTheme);
+githubTheme.headerFontFamily= ['Roboto','Segoe UI','sans-serif'];
+githubTheme.bodyFontFamily= ['Roboto', 'sans-serif'];
+const typography = new Typography(githubTheme);
 
 export default typography;
