d4789925780aa19a1591267486b1c20c419f178e
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-03-28 18:10:28 +0530

    Resolved issue of flicker

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,38 @@
-import 'babel-polyfill';
-import 'typeface-roboto';
-import React from 'react';
-
-import Navigation from '../components/navigation/navigation';
-import Ribbon from '../components/ribbon/ribbon';
-import withMui from '../components/withMUI/withMUI';
+import 'babel-polyfill'
+import 'typeface-roboto'
+import React, { Component } from 'react'
+import Navigation from '../components/navigation/navigation'
+import Ribbon from '../components/ribbon/ribbon'
+import withMui from '../components/withMUI/withMUI'
+import Spinner from '../components/Spinner/Snipper'
 // import styles from './index.module.css';
-const Index = ({ children, data }) => {
-  return (
-    <div>
-      <section>
-        <Navigation title={data.site.siteMetadata.title} />
-        {children()}
-      </section>
-    </div>
-  );
-};
+class Index extends Component {
+  state = {
+    loaded: false
+  }
 
-export default withMui(Index);
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      loaded: true
+    })
+  }
+
+  render() {
+    return this.state.loaded ? (
+      <div>
+        <section>
+          <Navigation title={this.props.data.site.siteMetadata.title} />
+          {this.props.children()}
+        </section>
+      </div>
+    ) : (
+      <div><Spinner /></div>
+    )
+  }
+}
+
+export default withMui(Index)
 export const query = graphql`
   query LayoutQuery {
     site {
@@ -26,4 +41,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
