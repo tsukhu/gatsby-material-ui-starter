c34713d7e2cd6f94d7fd8be964a1750937a6cd39
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-03-28 16:10:17 +0530

    injectTabEventPlugin reverted and bumped version

--- src/components/withMUI/withMUI.js
+++ src/components/withMUI/withMUI.js
@@ -1,17 +1,16 @@
-
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import createMuiTheme from 'material-ui/styles/createMuiTheme'
-// import injectTabEventPlugin from 'react-tap-event-plugin';
+import injectTabEventPlugin from 'react-tap-event-plugin'
 import purple from 'material-ui/colors/purple'
 import grey from 'material-ui/colors/grey'
 import cyan from 'material-ui/colors/cyan'
 
-// try {
-//   injectTabEventPlugin();
-// } catch(e) {
-//   // called once per app lifecycle
-// }
+try {
+  injectTabEventPlugin()
+} catch (e) {
+  // called once per app lifecycle
+}
 
 const withMaterialUI = ComposedComponent => {
   class HOC extends Component {
@@ -24,33 +23,33 @@ const withMaterialUI = ComposedComponent => {
             light: cyan[700],
             main: cyan[800],
             dark: cyan[900],
-            contrastText: '#fff',
+            contrastText: '#fff'
           },
           secondary: {
             light: grey[300],
             main: grey[800],
             dark: grey[900],
-            contrastText: '#E0E0E0',
-          },
+            contrastText: '#E0E0E0'
+          }
         },
         appBar: {
-          height: 50,
+          height: 50
         },
         toolbar: {
           titleFontSize: 'small',
           height: 40
         }
-      });
+      })
       return (
         <div>
           <MuiThemeProvider theme={muiTheme}>
             <ComposedComponent {...this.props} />
           </MuiThemeProvider>
         </div>
-      );
+      )
     }
   }
-  return HOC;
-};
+  return HOC
+}
 
-export default withMaterialUI;
+export default withMaterialUI
